3f73910ac127175be4dd40ad7ec83cb64e30d81a
Danny Granados <dgranados571@gmail.com>
2025-05-01 11:57:36 -0500

    Agrga al form registro RH Input Image para foto de perfil del RH

--- src/componentes/tvs_private/registraRH/registraRhForm.tsx
+++ src/componentes/tvs_private/registraRH/registraRhForm.tsx
@@ -1,6 +1,7 @@
-import React, { forwardRef, useImperativeHandle, useState } from 'react'
+import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 import "react-datepicker/dist/react-datepicker.css";
 import { IFormRHHandle } from '../../../models/IProps';
+import logoSvg from '../../../logo.svg'
 
 const RegistraRhForm: React.ForwardRefRenderFunction<IFormRHHandle> = ({ }, ref) => {
 
@@ -21,6 +22,7 @@ const RegistraRhForm: React.ForwardRefRenderFunction<IFormRHHandle> = ({ }, ref)
     const [correoCorporativo, setCorreoCorporativo] = useState('');
     const [celular, setCelular] = useState('');
     const [perfilProfesional, setPerfilProfesional] = useState('');
+    const [filesImages, setFilesImages] = useState<string[]>([logoSvg]);
 
     const [apellidosRef, setApellidosRef] = useState(false);
     const [nombresRef, setNombresRef] = useState(false);
@@ -29,6 +31,10 @@ const RegistraRhForm: React.ForwardRefRenderFunction<IFormRHHandle> = ({ }, ref)
     const [correoPersonalRef, setCorreoPersonalRef] = useState(false);
     const [correoCorporativoRef, setCorreoCorporativoRef] = useState(false);
     const [celularRef, setCelularRef] = useState(false);
+
+    const [fileRef, setFileRef] = useState(false);
+
+    const fileRefElementInput = useRef<HTMLInputElement | null>(null);
 
     const validateForm = () => {
         let formValidado = [];
@@ -55,6 +61,7 @@ const RegistraRhForm: React.ForwardRefRenderFunction<IFormRHHandle> = ({ }, ref)
         setCorreoPersonalRef(false)
         setCorreoCorporativoRef(false)
         setCelularRef(false)
+        setFileRef(false)
         if (formValidado.length === 0) {
             return {
                 prop0: apellidos,
@@ -88,12 +95,74 @@ const RegistraRhForm: React.ForwardRefRenderFunction<IFormRHHandle> = ({ }, ref)
         setCorreoPersonalRef(false)
         setCorreoCorporativoRef(false)
         setCelularRef(false)
+        setFileRef(false)
+    }
+
+    const eventInputFilesImages = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const fileList = e.target.files;
+        setFileRef(false)
+        if (fileList) {
+            const validImageTypes = ['image/jpeg', 'image/png'];
+            let controlTypeImage = true;
+            for (let i = 0; i < fileList.length; i++) {
+                if (!validImageTypes.includes(fileList[i].type)) {
+                    controlTypeImage = false
+                    break;
+                }
+            }
+            const base64Files: string[] = [];
+            if (controlTypeImage) {
+                let valorFinalMB = 0;
+                for (let step = 0; step < fileList.length; step++) {
+                    var fileSizeMB = fileList[step].size / 1024 / 1024;
+                    valorFinalMB = valorFinalMB + fileSizeMB;
+                }
+                if (valorFinalMB < 10) {
+                    Array.from(fileList).forEach((file) => {
+                        const reader = new FileReader();
+                        reader.onloadend = () => {
+                            if (reader.result && typeof reader.result === 'string') {
+                                base64Files.push(reader.result);
+                                if (base64Files.length === fileList.length) {
+                                    setFilesImages(base64Files);
+                                }
+                            }
+                        };
+                        reader.readAsDataURL(file);
+                    });
+                } else {
+                    base64Files.push(logoSvg);
+                    setFilesImages(base64Files);
+                    setFileRef(true)
+                }
+            } else {
+                base64Files.push(logoSvg);
+                setFilesImages(base64Files);
+                setFileRef(true)
+            }
+        }
     }
 
     return (
         <>
-            <h4>Información del recurso humano</h4>
-            <p>A continuación, ingresa la información del recurso humano:</p>
+            <div className="div-perfil-registra-rh">
+                <div className="div-perfil-data">
+                    <h4>Información del recurso humano</h4>
+                    <p>A continuación, ingresa la información del recurso humano:</p>
+                </div>
+                <div className="div-perfil-image">
+                    <img src={filesImages[0]} className='img-perfil-rh' />
+                    <h4>Seleccionar foto</h4>
+                    <input ref={fileRefElementInput} type="file" multiple onChange={(e) => eventInputFilesImages(e)} className={fileRef ? 'form-control form-control-error' : 'form-control'} />
+                    {
+                        fileRef ?
+                            <p>**El archivo seleccionado no es valido**</p>
+                            :
+                            <></>
+                    }
+                </div>
+            </div>
+            <hr />
             <div className="row">
                 <div className="col-12 col-sm-12 col-md-6 col-lg-6" >
                     <div className='div-form'>
